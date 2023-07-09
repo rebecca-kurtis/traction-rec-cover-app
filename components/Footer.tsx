b209fa38bafadb5ada14c575e201fc3a1a77c018
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-07-09 13:43:59 -0700

    Add footer icons and styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,23 +2,28 @@ import React from 'react';
 import styles from "../styles/Footer.module.scss";
 import Image from "next/image";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faLinkedIn } from '@fortawesome/free-solid-svg-icons'
+// import { faLinkedIn } from '';
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
 
 interface FooterProps {
 
 }
 
 export const Footer: React.FC<FooterProps> = ({}) => {
+  let linkedInUrl = "https://www.linkedin.com/in/rebeccakurtis/";
+  let githubUrl = "https://github.com/rebecca-kurtis/";
     return (
       <div className={styles.footerContainer}>
-        Footer
-        <Image
-          className={styles.footerIcons}
-          src="/images/linkedin-white.svg"
-          alt=""
-          width={50}
-          height={50}
-        />
+        <div className={styles.footerIconContainer}>
+         <a href={linkedInUrl} target="__blank" > <FaLinkedin className={styles.footerIcons} /></a>
+         <a href={githubUrl} target="__blank" > <FaGithub className={styles.footerIcons} /></a>
+        </div>
+        <div className={styles.footerCopyrightContainer}>
+        <p className={styles.copyrightText}>© 2023 Rebecca Kurtis</p>
+        </div>
+        
       </div>
     );
 }
