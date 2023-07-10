494d2b7e371d09aaedb4e9ca67d55877ba5e3a1d
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-07-09 22:42:34 -0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import styles from '../styles/burger.module.scss';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,8 +14,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  let googleUrl = "https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600&family=Shrikhand&display=swap";
+
   return (
     <html lang="en">
+       <Head>
+       <link href={googleUrl} rel="stylesheet"></link> 
+        </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Script from 'next/script'
 import Head from 'next/head';
 import styles from './page.module.scss';
 import { Header } from '../../components/Header';
@@ -11,14 +12,19 @@ import { WhyMe } from '../../components/WhyMe';
 import { Skills } from '../../components/Skills';
 
 export default function Home() {
+  let googleUrl = "https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600&family=Shrikhand&display=swap";
+
   return (
     <div className={styles.main}>
       <Head>
-        {/* <title>Rebecca Kurtis Portfolio</title>
+        <title>Rebecca Kurtis Portfolio</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="use-credentials" />*/}
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600&family=Shrikhand&display=swap" rel="stylesheet"></link> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="use-credentials" />
+        <link href={googleUrl} rel="stylesheet"></link> 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
+
       </Head>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></Script>
       <Header></Header>
       <WelcomeHeader></WelcomeHeader>
       <About></About>
