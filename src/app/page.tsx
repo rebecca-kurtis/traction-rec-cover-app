356b656b3f310d1c5aff4a840d6b4297d7894de5
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-07-09 22:22:09 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
       <Head>
         {/* <title>Rebecca Kurtis Portfolio</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="use-credentials" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600&family=Shrikhand&display=swap" rel="stylesheet"></link> */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="use-credentials" />*/}
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600&family=Shrikhand&display=swap" rel="stylesheet"></link> 
       </Head>
       <Header></Header>
       <WelcomeHeader></WelcomeHeader>
