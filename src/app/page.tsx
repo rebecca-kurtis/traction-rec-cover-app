85b893615708c5a1ab2b290ee894d671d6ab79bf
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-07-10 15:06:24 -0700

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     <div className={styles.main}>
       <Head>
         <title>Rebecca Kurtis Portfolio</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -62,7 +62,7 @@ export default function Home() {
   href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
   integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
   crossOrigin="anonymous"
-/>
+/> */}
       </Head>
       <Header></Header>
       <WelcomeHeader></WelcomeHeader>
